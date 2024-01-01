b4a4e2ad4c696522a83b41ea54389171bb94eb45
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-01-01 20:59:11 +0100

    chore: add useReducer to context

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -11,10 +11,16 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
   const { cityName, country, emoji, date, id, notes, position } = city;
   const { lat, lng } = position;
   // console.log(city);
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
+
   return (
     <li>
       <Link
@@ -26,7 +32,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
